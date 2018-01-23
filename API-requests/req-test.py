979363440d96942fa760c9f6fc55e65bc378a1fa
Vishal Basra <v.basra@cvent.com>
2018-01-23 12:46:00 +0530

    Commit initiated at Tue Jan 23 12:46:00 IST 2018

--- API-requests/req-test.py
+++ API-requests/req-test.py
@@ -1,3 +1,3 @@
 import requests, pprint
-r = requests.get('https://af1rouncri.execute-api.us-east-1.amazonaws.com/dev/hello-world')
+r = requests.get('https://XYXYXYXYX.execute-api.us-east-1.amazonaws.com/dev/hello-world')
 pprint.pprint(r.json())
