95768bf961c91f73dad47a986ac9b19438fb25b3
Vishal Basra <v.basra@cvent.com>
2018-01-23 12:57:29 +0530

    Commit initiated at Tue Jan 23 12:57:29 IST 2018

--- node/serverless/hello-world/handler.js
+++ node/serverless/hello-world/handler.js
@@ -14,3 +14,21 @@ module.exports.helloWorld = (event, context, callback) => {
 
   callback(null, response);
 };
+
+module.exports.helloName = (event, context, callback) => {
+  let name = 'stranger';
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    name = event.queryStringParameters.name;
+  }
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: `Hello, ${name}!`
+    })
+  };
+
+  callback(null, response);
+}
